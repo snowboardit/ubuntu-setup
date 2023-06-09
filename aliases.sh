1c58bc7a0714b81f205e17ce2446031f78ec6db2
Max <max@maxlareau.com>
2023-06-08 21:35:04 -0400

    Add aliases to bashrc script

---
+++ aliases.sh
@@ -0,0 +1,8 @@
+echo 'alias cls="clear -x"'
+echo 'alias gs="git status"' >> ~/.bashrc
+echo 'alias gf="git fetch"' >> ~/.bashrc
+echo 'alias ga="git add ."' >> ~/.bashrc
+echo 'alias gc="git commit"' >> ~/.bashrc
+echo 'alias gpl="git pull"' >> ~/.bashrc
+echo 'alias gps="git push"' >> ~/.bashrc
+source ~/.bashrc
